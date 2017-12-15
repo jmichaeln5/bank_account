bec3820ef722eb68d24671c724d76d7f6abeb789
Jonathan Norton <Jmichaeln5@gmail.com>
2017-12-15 10:19:08 -0500

    stage one bank account

---
+++ bank_account.rb
@@ -0,0 +1,23 @@
+class Bank
+  attr_accessor: bank_account
+
+    def initialize bank_account
+      @bank_account = bank_account
+    end
+
+    def balance
+      @balance
+    end
+
+end
+
+
+
+
+
+#
+# Utilize the basic building blocks of Ruby's object model including:
+# attribute (attr_) access methods
+# initialize
+# @instance variables and methods
+# @@class variables and self.methods
